0420f995c209bb4bb66e2f2c5f029fcdb4979ed6
karimTDiaz <karimtilouni94@gmail.com>
2024-07-28 15:26:48 +0200

    (components) : create header and logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Logo = () => {
+  return <StyledLogo src="/assets/logos/logo.svg" />;
+};
+
+const StyledLogo = styled.img`
+  width: 4.375rem;
+`;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Logo } from '../../components/logo/Logo';
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo />
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  padding: 1rem;
+`;
