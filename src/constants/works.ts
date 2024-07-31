29761a1a484834f13ee9c6cdea1eee267d3bf78f
karimTDiaz <karimtilouni94@gmail.com>
2024-08-01 00:18:51 +0200

    fix : src in works images

--- src/constants/works.ts
+++ src/constants/works.ts
@@ -2,19 +2,19 @@ export const WORKS = [
   {
     name: 'JTILOUNI',
     number: '01',
-    preview: 'assets/images/tln.png',
+    preview: '/assets/images/tln.png',
     type: 'Portfolio-web'
   },
   {
     name: 'OTHER LIFE STORE',
     number: '02',
-    preview: 'assets/images/otf.png',
+    preview: '/assets/images/otf.png',
     type: 'MERN App'
   },
   {
     name: 'Stream Pulse',
     number: '03',
-    preview: 'assets/images/ntr.jpg',
+    preview: '/assets/images/ntr.jpg',
     type: 'Movies App'
   }
 ];
